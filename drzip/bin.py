5a8874d6d453bc1ffb3c54abdc8fb2f94710c76b
Dmitry Ryzhkov <dmitry@microsoft.com>
2019-05-26 17:53:29 -0700

    aaded decompressing

--- drzip/bin.py
+++ drzip/bin.py
@@ -1,26 +1,22 @@
 from bitarray import bitarray
 
+
 class Packer:
 
     def __init__(self):
         self.bits = []
 
-
     def int8(self, integer):
         self.bits += self._int_to_bit(integer, 8)
-
 
     def int32(self, integer):
         self.bits += self._int_to_bit(integer, 32)
 
-
     def addBits(self, bits):
         self.bits += bits
 
-
     def pack(self):
-        return bitarray(self.bits).tobytes()
-
+        return bitarray(self.bits, endian='little').tobytes()
 
     def print(self):
         print(self.bits)
@@ -28,10 +24,48 @@ class Packer:
 
     def _int_to_bit(self, integer, bit_count):
         bits = []
-        count = 0
-        while count < bit_count:
-            count += 1
+        while bit_count > 0:
+            bit_count -= 1
             bits.append(integer & 0x1)
             integer = integer >> 1
 
         return bits
+
+
+class Unpacker:
+
+    def __init__(self, byte_data):
+        self.bits = bitarray(endian='little')
+        self.bits.frombytes(byte_data)
+
+    def int32(self):
+        return self.bit_to_int(32)
+
+    def int8(self):
+        return self.bit_to_int(8)
+
+    def bit_to_int(self, bit_count):
+        res = 0
+        bits = self.pop_bits(bit_count)
+        bits.reverse()
+
+        for bit in bits:
+            res = res << 1
+            res = res | bit
+
+        return res
+
+    def pop_bits(self, count):
+        bits = []
+        while count > 0:
+            bits.append(self.bits.pop(0))
+            count -= 1
+
+        return bits
+
+    def seek(self, count):
+        return [x for x in self.bits[0: count]]
+
+    def print(self):
+        print(self.bits)
+        print(len(self.bits))
--- drzip/drzip.py
+++ drzip/drzip.py
@@ -2,13 +2,14 @@
 import argparse
 import sys
 from operator import attrgetter
-from bin import Packer
+from bin import Packer, Unpacker
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--compress', action='store_true', help='to compress data')
 parser.add_argument('--decompress', action='store_true',
                     help='to decompress data')
 args = parser.parse_args()
+
 
 class Node:
 
@@ -24,11 +25,18 @@ class Node:
     def print(self):
         print(self.__repr__())
 
-        if self.left != None: 
+        if self.left != None:
             self.left.print()
-        
+
         if self.right != None:
             self.right.print()
+
+
+"""Uses Huffman Coding Algorithm to compress data.
+Compress data format:
+    - first 32 bits - length of original content
+    - 
+"""
 
 
 def compress(original):
@@ -36,15 +44,15 @@ def compress(original):
     bits_table = {}
     build_table(huff_tree, bits_table)
     packer = Packer()
-    packer.int32(len(original)) # pack length of original data as first 32 bits
+    # pack length of original data as first 32 bits
+    packer.int32(len(original))
     pack_table(bits_table, packer)
     pack_original(original, packer, bits_table)
-    packer.print()
     return packer.pack()
 
 
 def pack_table(table, packer):
-    packer.int8(len(table)) #pack length of lookup table as frist 8 bits
+    packer.int8(len(table))  # pack length of lookup table as frist 8 bits
     for byte, path in table.items():
         packer.int8(byte)
         packer.int8(len(path))
@@ -59,8 +67,38 @@ def pack_original(data, packer, lookup):
             raise Exception("byte not found in lookup table")
 
 
+def unpack_table(unpacker):
+    table_len = unpacker.int8()
+    table = {}
+    i = 0
+    while i < table_len:
+        byte = unpacker.int8()
+        path_len = unpacker.int8()
+        path = unpacker.pop_bits(path_len)
+        table[byte] = [int(p) for p in path]
+        i += 1
+
+    return table
+
+
 def decompress(compressed):
-    print(compressed)
+    unpacker = Unpacker(compressed)
+    data_len = unpacker.int32()
+    lookup_table = unpack_table(unpacker)
+    res = ""
+    while data_len > 0:
+        data_len -= 1
+        byte = lookup_bits(lookup_table, unpacker)
+        res += chr(byte)
+    return res
+
+
+def lookup_bits(table, unpacker):
+    for (byte, path) in table.items():
+        if unpacker.seek(len(path)) == path:
+            unpacker.pop_bits(len(path))
+            return byte
+    raise Exception('data not found')
 
 
 def build_tree(data):
@@ -71,9 +109,9 @@ def build_tree(data):
             count_map[byte] += 1
         else:
             count_map[byte] = 1
-    
+
     counter_list = [Node(v, k) for k, v in count_map.items()]
-    
+
     while len(counter_list) > 1:
         min_left = min(counter_list, key=attrgetter('count'))
         counter_list.remove(min_left)
@@ -85,22 +123,24 @@ def build_tree(data):
         parent.right = min_right
 
         counter_list.append(parent)
-    
+
     return counter_list.pop()
 
 
-def build_table(node, table, path = []):
-    if node.left == None and node.right == None: # left
+def build_table(node, table, path=[]):
+    if node.left == None and node.right == None:  # left
         table[node.byte] = path
     else:
         build_table(node.left, table, path + [0])
         build_table(node.right, table, path + [1])
+
 
 if __name__ == "__main__":
     c = vars(args)["compress"]
     d = vars(args)["decompress"]
 
     if c == True:
-        print(compress(bytes(sys.stdin.read(), "utf-8")))
+        output_bytes = compress(bytes(sys.stdin.read(), "utf-8"))
+        sys.stdout.buffer.write(output_bytes)
     else:
-        decompress(bytes(sys.stdin.read(), "utf-8"))
+        print(decompress(bytes(sys.stdin.buffer.read())))
